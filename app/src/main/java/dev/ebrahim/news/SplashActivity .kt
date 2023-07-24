535c27914f627ca953db47e72df3c7183f2f65d6
EbrahimHeggy <85329448+EbrahimHeggy@users.noreply.github.com>
2023-07-25 01:45:01 +0300

    Retrieve All Clubs Done

---
+++ app/src/main/java/dev/ebrahim/news/SplashActivity .kt
@@ -0,0 +1,28 @@
+package dev.ebrahim.news
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import dev.ebrahim.news.MainActivity
+import dev.ebrahim.news.R
+
+class SplashActivity : AppCompatActivity() {
+    val SPLASH_DELAY: Long = 3000 // 3 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        // Delay the splash screen for 3 seconds
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Start the main activity here
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            // Finish the splash activity
+            finish()
+        }, SPLASH_DELAY)
+    }
+}
